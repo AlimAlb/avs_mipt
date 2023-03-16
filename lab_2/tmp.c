121131d640ad3f06d6a7d8287680173ecd55e1a1
Alim Albogachiev <alimalbogachiev@yandex.ru>
2023-03-16 17:32:46 +0300

    lab2 - done

--- lab_2/tmp.c
+++ lab_2/tmp.c
@@ -11,47 +11,45 @@ int used[0x10000] = {0};
 
 // check on real numbers
 
-long long to_number(long long cell_addr){
-    if (MEMORY[cell_addr] / 0x10000000000000 / 8  == 0){
-        return MEMORY[cell_addr];
+long long to_number(long long num){
+    if (num / 0x10000000000000 / 8  == 0){
+        return num;
     }
     else{
-        long long tmp = ~(MEMORY[cell_addr]-1) & 0x00FFFFFFFFFFFFFF;
+        long long tmp = ~(num-1) & 0x00FFFFFFFFFFFFFF;
         tmp = -tmp;
         return tmp;
     }
 }
 
 void add(int addr1, int addr2, int addr3){
-    printf("%d + %d\n", addr1, addr2);
+    printf("(addr1->%.4x, value->%lld)  +  (addr2->%.4x, value->%lld) ---> ", addr1, to_number(MEMORY[addr1]), addr2, to_number(MEMORY[addr2]));
     MEMORY[addr3] = to_number(addr1) + to_number(addr2);
-    
-    printf("%.4x -- %lld + %lld\n", addr3, to_number(addr1), to_number(addr2));
+    printf("result_addr->%.4x, value->%lld\n", addr3, to_number(MEMORY[addr3]));
 }
 
 void diffirence(int addr1, int addr2, int addr3){
+    printf("(addr1->%.4x, value->%lld)  -  (addr2->%.4x, value->%lld) ---> ", addr1, to_number(MEMORY[addr1]), addr2, to_number(MEMORY[addr2]));
     MEMORY[addr3] = to_number(addr1) - to_number(addr2);
-    printf("%.4x -- %lld - %lld\n", addr3, to_number(addr1), to_number(addr2));
-}
+    printf("result_addr->%.4x, value->%lld\n", addr3, to_number(MEMORY[addr3]));
+    }
 
 void mult(int addr1, int addr2, int addr3){
-    printf("addr1 - %x, value - %lld,  addr2 - %x, value - %lld\n", addr1, MEMORY[addr1], addr2, MEMORY[addr2]);
-
-    MEMORY[addr3] = to_number(addr1) * to_number(addr2);
-    printf("%.4x -- %lld * %lld = %lld\n", addr3, to_number(addr1), to_number(addr2), MEMORY[addr3]);
+    printf("(addr1->%.4x, value->%lld)  *  (addr2->%.4x, value->%lld) ---> ", addr1, to_number(MEMORY[addr1]), addr2, to_number(MEMORY[addr2]));
+    MEMORY[addr3] = to_number(MEMORY[addr1]) * to_number(MEMORY[addr2]);
+    printf("result_addr->%.4x, value->%lld\n", addr3, to_number(MEMORY[addr3]));
 }
 
 void division(int addr1, int addr2, int addr3){
-    printf("addr1 - %x, value - %llx,  addr2 - %x, value - %lld\n", addr1, MEMORY[addr1], addr2, MEMORY[addr2]);
-    printf("%lld\n", ~(MEMORY[addr1]-1));
-    MEMORY[addr3] =to_number(addr1) / to_number(addr2);
-    MEMORY[addr3 + 1] = to_number(addr1) % to_number(addr2);
-    printf("%.4x -- %lld / %lld\n", addr3, to_number(addr1), to_number(addr2));
+    printf("(addr1->%.4x, value->%lld)  /  (addr2->%.4x, value->%lld) ---> ", addr1, to_number(MEMORY[addr1]), addr2, to_number(MEMORY[addr2]));
+    MEMORY[addr3] = to_number(MEMORY[addr1]) / to_number(MEMORY[addr2]);
+    MEMORY[addr3 + 1] = to_number(MEMORY[addr1]) % to_number(MEMORY[addr2]);
+    printf("result_addr_1->%.4x, value->%lld // result_addr_2->%.4x, value->%lld\n", addr3, to_number(MEMORY[addr3]), addr3+1, to_number(MEMORY[addr3+1]));
 }
 
 long long operation(int code_of_operation,int a1,int a2,int a3){
     long long coop = code_of_operation;
-    coop = coop << 48;
+    coop = coop << 48; 
     long long addr_1 = a1;
     addr_1 = addr_1 << 32;
     long long addr_2 = a2;
@@ -103,16 +101,28 @@ int run_operation(int* opers,int curr_cell){
 
         case 0x80: return opers[3];
 
-        case 0x81: return opers[1] == opers[2] ? opers[3] : curr_cell+1;
-        case 0x82: return opers[1] != opers[2] ? opers[3] : curr_cell+1;
-        case 0x83: return opers[1] < opers[2] ? opers[3] : curr_cell+1;
-        case 0x84: return opers[1] >= opers[2] ? opers[3] : curr_cell+1;
-        case 0x85: return opers[1] <= opers[2] ? opers[3] : curr_cell+1;
-        case 0x86: return opers[1] > opers[2] ? opers[3] : curr_cell+1;
+        case 0x81: 
+            printf("if (addr1->%.4x, value->%lld) == (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] == MEMORY[opers[2]] ? opers[3] : curr_cell+1;
+        case 0x82:
+            printf("if (addr1->%.4x, value->%lld) != (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] != MEMORY[opers[2]] ? opers[3] : curr_cell+1;
+        case 0x83:
+            printf("if (addr1->%.4x, value->%lld) < (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] < MEMORY[opers[2]] ? opers[3] : curr_cell+1;
+        case 0x84:
+            printf("if (addr1->%.4x, value->%lld) >= (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] >= MEMORY[opers[2]] ? opers[3] : curr_cell+1;
+        case 0x85:
+            printf("if (addr1->%.4x, value->%lld) <= (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] <= MEMORY[opers[2]] ? opers[3] : curr_cell+1;
+        case 0x86:
+            printf("if (addr1->%.4x, value->%lld) > (addr2->%.4x,value->%lld) then go to %.4x\n", opers[1], MEMORY[opers[1]], opers[2], MEMORY[opers[2]], opers[3]);
+            return MEMORY[opers[1]] < MEMORY[opers[2]] ? opers[3] : curr_cell+1;
 
-        case 0x00: MEMORY[opers[3]] = MEMORY[opers[1]]; return curr_cell+1;
+        case 0x00: printf("(addr1->%.4x, value->%lld) := (addr2->%.4x,value->%lld)\n", opers[3], MEMORY[opers[3]], opers[1], MEMORY[opers[3]]); MEMORY[opers[3]] = MEMORY[opers[1]]; return curr_cell+1;
 
-        case 0x99: return sizeof(MEMORY)/8;
+        case 0x99: printf("code 99 ----> end of execution"); return sizeof(MEMORY)/8;
     }
 }
 
@@ -120,6 +130,7 @@ void exec(){
     int curr_cell = 0;
     do{
         int opers[4] = {0};
+        printf("current memory cell --> %.4x ---- ", curr_cell);
         curr_cell = run_operation(decompose_operation(curr_cell, opers), curr_cell);
     } while (curr_cell < sizeof(MEMORY) / 8);
 }
@@ -218,18 +229,25 @@ void show_memory(){
     
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    read_code("formula_code.txt");
-    
+    printf("\ncode file ---> %s\n", argv[1]);
+    read_code(argv[1]);
+    printf("\n↓↓Memory before the execution↓↓\n\n");
+    show_memory();
+    printf("\n");
+    printf("\n-------------start of execution-------------\n\n");
     exec();
-    printf("%lld\n", MEMORY[0x103]);
+    printf("\n\n-------------end of execution-------------\n\n");
+    printf("\n↓↓Memory at the end of execution↓↓\n\n");
     show_memory();
     // MEMORY[100] = 0xfffffffffffe999b;
     // int opers[4] = {0}; 
     // decompose_operation(100, opers);
     // printf("%x - %x - %x - %x", opers[0], opers[1], opers[2], opers[3]);
     // printf("%d", sizeof(long long)*2-2);
+    
+    
 }
 
 
